98649ac0a0059ed56fc45ce616b702416f28c92c
Andrew Hoang <andrewhoang03@gmail.com>
2018-05-31 17:33:54 -0700

    Changed to ping us-west-2 (Orgeon)

--- get_sentiment.py
+++ get_sentiment.py
@@ -3,7 +3,7 @@ import json
 
 def get_sentiment(text):
 
-  comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+  comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
   response = comprehend.detect_sentiment(Text=text, LanguageCode='en')
   print(response)
   return response["Sentiment"].lower()
